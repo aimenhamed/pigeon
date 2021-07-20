6271386bfd0f3a6f7d15a7d0030187023a06ef66
Aimen <aimenhamed@live.com.au>
2021-07-20 11:02:44 +1000

    added login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
-
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+
+    if (!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="c5480421-5419-43fc-8a6e-727aba1a9402"
-            userName="tomisme"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
